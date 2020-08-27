8d5f02446f03824fca861f6a0856df30e73e9db4
Abhishek Yadav <abhishekyadav9844221505@gmail.com>
2020-08-27 05:48:45 -0700

    SOlved the second quiz

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const fName = "Abhishek";
-const lName = "Yadav";
-//js expresion
-// ReactDom.render(
-//   <h1>
-//     My first name is {fName} and my last name is {lName} .
-//   </h1>,
-//   document.getElementById("root")
-// );
-
-//js template literals
+let dateobj = String(new Date());
+let dateobj2 = new Date();
 ReactDom.render(
-  <h1>{`My first name is ${fName} and my last name is ${lName} !got it. !!!!! and my favourite number is ${Math.random()} and my favourite sum is ${
-    4 + 4
-  }`}</h1>,
+  <>
+    <h1>Hello Abhi</h1>
+    <p>Toady's date is {`${dateobj.slice(0, 15)}`}</p>
+    <p>Current Time is {`${dateobj.slice(16, 24)}`}</p>
+
+    <h1>As expected</h1>
+    <p>Toady's date is {`${dateobj2.toLocaleDateString()}`}</p>
+    <p>Toady's date is {`${dateobj2.toLocaleTimeString()}`}</p>
+  </>,
   document.getElementById("root")
 );
