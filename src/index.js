8779d60def2d9ac302e1d9cbb6115e370cc1db64
Abhishek Yadav <abhishekyadav9844221505@gmail.com>
2020-08-27 04:41:44 -0700

    Added custom code in index.js to display data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-//Importing React from converting our jsx to browser understandable js
-import React from "react";
-//importing react-dom for manipulating dom
-import ReactDom from "react-dom";
-//main program
-// ReactDom.render(what to show, where to show,callback function);
-ReactDom.render(<h1>Hello World Abhi.</h1>, document.getElementById("root"));
+// import React from "react";
+// import ReactDom from "react-dom";
+
+//React code which get translated using babel
+// ReactDom.render(<h1>Hello World Abhi.</h1>, document.getElementById("root"));
+
+//pure code translated by babel
+// ReactDom.render(
+//   /*#__PURE__*/ React.createElement("h1", null, "Hello World Abhi."),
+//   document.getElementById("root")
+// );
+
+//custom code
+let h1 = document.createElement("h1");
+h1.innerHTML = "Hello Abhi";
+document.getElementById("root").appendChild(h1);
