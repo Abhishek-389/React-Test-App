ba2384c097547cf8c64f59979495c5d17636da90
Abhishek Yadav <abhishekyadav9844221505@gmail.com>
2020-08-27 05:13:02 -0700

    Used js expression and template literals.

--- src/index.js
+++ src/index.js
@@ -1,68 +1,20 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-//Rendering multiple element
-//method1==>Using a div
+const fName = "Abhishek";
+const lName = "Yadav";
+//js expresion
 // ReactDom.render(
-//   <div>
-//     <h1>Top 10 Netflix shows</h1>
-//     <p>These are my top ten Netflix seried</p>
-//     <ol>
-//       <li>Never Have I Ever</li>
-//       <li>The Baby-Sitters Club</li>
-//       <li>The Umbrella Academy</li>
-//       <li>Russian Doll</li>
-//       <li>Unbreakable Kimmy Schmidt</li>
-//     </ol>
-//   </div>,
+//   <h1>
+//     My first name is {fName} and my last name is {lName} .
+//   </h1>,
 //   document.getElementById("root")
 // );
 
-//method2==>Using an array  only on above react 16 version
-/*ReactDom.render(
-  [
-    <h1>Top 10 Netflix shows</h1>,
-    <p>These are my top ten Netflix seried</p>,
-    <ol>
-      <li>Never Have I Ever</li>
-      <li>The Baby-Sitters Club</li>
-      <li>The Umbrella Academy</li>
-      <li>Russian Doll</li>
-      <li>Unbreakable Kimmy Schmidt</li>
-      <li>Breakable Kimmy Schmidt</li>
-    </ol>,
-  ],
-  document.getElementById("root")
-); */
-
-//method3==>Using  --React.Fragment--
-// ReactDom.render(
-//   <React.Fragment>
-//     <h1>Top 10 Netflix shows</h1>
-//     <p>These are my top ten Netflix seried</p>
-//     <ol>
-//       <li>Never Have I Ever</li>
-//       <li>The Baby-Sitters Club</li>
-//       <li>The Umbrella Academy</li>
-//       <li>Russian Doll</li>
-//       <li>Unbreakable Kimmy Schmidt</li>
-//     </ol>
-//   </React.Fragment>,
-//   document.getElementById("root")
-// );
-
-//method4==>Using  --React.Fragment Syntactic sugar --
+//js template literals
 ReactDom.render(
-  <>
-    <h1>Top 10 Netflix shows</h1>
-    <p>These are my top ten Netflix seried</p>
-    <ol>
-      <li>Never Have I Ever</li>
-      <li>The Baby-Sitters Club</li>
-      <li>The Umbrella Academy</li>
-      <li>Russian Doll</li>
-      <li>Breakable Kimmy Schmidt</li>
-    </ol>
-  </>,
+  <h1>{`My first name is ${fName} and my last name is ${lName} !got it. !!!!! and my favourite number is ${Math.random()} and my favourite sum is ${
+    4 + 4
+  }`}</h1>,
   document.getElementById("root")
 );
