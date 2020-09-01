c66b797153adc1531d096a840daa971ec6f10cec
Abhishek Yadav <abhishekyadav9844221505@gmail.com>
2020-09-01 05:16:50 -0700

    Added internal and inline css using js objects

--- src/index.js
+++ src/index.js
@@ -5,10 +5,34 @@ import "./index.css";
 const fImage = "https://source.unsplash.com/300x300/?nature,water";
 const sImage = "https://source.unsplash.com/300x300/?water,nature";
 const tImage = "https://source.unsplash.com/300x300/?nature,beauty";
+//interal css
+const heading = {
+  width: "30%",
+  color: "#192a56",
+  margin: "50px auto",
+  fontWeight: "800",
+  padding: "10px",
+  fontFamily: '"Noto Sans TC", sans-serif',
+  boxShadow: "2px 2px 3px rgb(107, 100, 100), -2px -2px 3px rgb(107, 100, 100)",
+};
 
 ReactDom.render(
   <>
-    <h1 className="heading">Welcome to Abhi Gallery </h1>
+    <h1
+    //inline  css
+      style={{
+        width: "30%",
+        color: "#192a56",
+        margin: "50px auto",
+        fontWeight: "800",
+        padding: "10px",
+        fontFamily: '"Noto Sans TC", sans-serif',
+        boxShadow:
+          "2px 2px 3px rgb(107, 100, 100), -2px -2px 3px rgb(107, 100, 100)",
+      }}
+    >
+      Welcome to Abhi Gallery{" "}
+    </h1>
     <div className="imageContainer">
       <img src={fImage} alt="" />
       <img src={sImage} alt="" />
