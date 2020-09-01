90862393f1e12961da7f23002cae43d254375862
Abhishek Yadav <abhishekyadav9844221505@gmail.com>
2020-09-01 03:44:01 -0700

    JSX props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-let dateobj = String(new Date());
-let dateobj2 = new Date();
+const fImage = "https://source.unsplash.com/300x300/?nature,water";
+const sImage = "https://source.unsplash.com/300x300/?water,nature";
+const tImage = "https://source.unsplash.com/300x300/?nature,beauty";
+
 ReactDom.render(
   <>
-    <h1>Hello Abhi</h1>
-    <p>Toady's date is {`${dateobj.slice(0, 15)}`}</p>
-    <p>Current Time is {`${dateobj.slice(16, 24)}`}</p>
-
-    <h1>As expected</h1>
-    <p>Toady's date is {`${dateobj2.toLocaleDateString()}`}</p>
-    <p>Toady's date is {`${dateobj2.toLocaleTimeString()}`}</p>
+    <h1>Welcome to Abhi Gallery </h1>
+    <img src={fImage} alt="" />
+    <img src={sImage} alt="" />
+    <img src={tImage} alt="" />
   </>,
   document.getElementById("root")
 );
