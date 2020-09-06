a87a3b3aae6e3214af7911efddac4c4da5c0fcb3
Abhishek Yadav <abhishekyadav9844221505@gmail.com>
2020-09-06 02:32:35 -0700

    React Component

---
+++ src/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function List() {
+  return (
+    <ol>
+      <li>Abhi</li>
+      <li>Aadit</li>
+      <li>Arjun</li>
+      <li>Shahi</li>
+    </ol>
+  );
+}
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
-const currentHour = new Date().getHours();
-let greeting = "";
-let cssStyle = {};
 
-if (currentHour >= 1 && currentHour < 12) {
-  greeting = "Good Morning";
-  cssStyle.color = "green";
-} else if (currentHour >= 12 && currentHour < 19) {
-  greeting = "Good Afternoon";
-  cssStyle.color = "Orange";
-} else {
-  greeting = "Good Night";
-  cssStyle.color = "black";
-}
-
-ReactDom.render(
-  <>
-    <h1 className="heading">
-      Welcome Sir
-      <span style={cssStyle}>{" " + greeting}</span>
-    </h1>
-  </>,
-
-  document.getElementById("root")
-);
+import App from "./App";
+ReactDom.render(<App />, document.getElementById("root"));
