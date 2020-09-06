7738da302d0f26302e0d3cd83b6d5cd3c55726fb
Abhishek Yadav <abhishekyadav9844221505@gmail.com>
2020-09-05 22:54:13 -0700

    Complteted the quix

--- src/index.js
+++ src/index.js
@@ -1,43 +1,27 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
+const currentHour = new Date().getHours();
+let greeting = "";
+let cssStyle = {};
 
-const fImage = "https://source.unsplash.com/300x300/?nature,water";
-const sImage = "https://source.unsplash.com/300x300/?water,nature";
-const tImage = "https://source.unsplash.com/300x300/?nature,beauty";
-//interal css
-const heading = {
-  width: "30%",
-  color: "#192a56",
-  margin: "50px auto",
-  fontWeight: "800",
-  padding: "10px",
-  fontFamily: '"Noto Sans TC", sans-serif',
-  boxShadow: "2px 2px 3px rgb(107, 100, 100), -2px -2px 3px rgb(107, 100, 100)",
-};
+if (currentHour >= 1 && currentHour < 12) {
+  greeting = "Good Morning";
+  cssStyle.color = "green";
+} else if (currentHour >= 12 && currentHour < 19) {
+  greeting = "Good Afternoon";
+  cssStyle.color = "Orange";
+} else {
+  greeting = "Good Night";
+  cssStyle.color = "black";
+}
 
 ReactDom.render(
   <>
-    <h1
-    //inline  css
-      style={{
-        width: "30%",
-        color: "#192a56",
-        margin: "50px auto",
-        fontWeight: "800",
-        padding: "10px",
-        fontFamily: '"Noto Sans TC", sans-serif',
-        boxShadow:
-          "2px 2px 3px rgb(107, 100, 100), -2px -2px 3px rgb(107, 100, 100)",
-      }}
-    >
-      Welcome to Abhi Gallery{" "}
+    <h1 className="heading">
+      Welcome Sir
+      <span style={cssStyle}>{" " + greeting}</span>
     </h1>
-    <div className="imageContainer">
-      <img src={fImage} alt="" />
-      <img src={sImage} alt="" />
-      <img src={tImage} alt="" />
-    </div>
   </>,
 
   document.getElementById("root")
