ce004db7e29c68143acb7fa3f52763283d1bc021
Abhishek Yadav <abhishekyadav9844221505@gmail.com>
2020-09-08 20:37:12 -0700

    Added the full responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -6,38 +6,14 @@ function App() {
   return (
     <>
       <div className="cards">
-        <Card
-          imageSrc={data[0].imageSrc}
-          category={data[0].category}
-          title={data[0].title}
-          link={data[0].link}
-        />
-        <Card
-          imageSrc={data[1].imageSrc}
-          category={data[1].category}
-          title={data[1].title}
-          link={data[1].link}
-        />
-        <Card
-          imageSrc={data[2].imageSrc}
-          category={data[2].category}
-          title={data[2].title}
-          link={data[2].link}
-        />
-      </div>
-      <div className="cards">
-        <Card
-          imageSrc={data[3].imageSrc}
-          category={data[3].category}
-          title={data[3].title}
-          link={data[3].link}
-        />
-        <Card
-          imageSrc={data[4].imageSrc}
-          category={data[4].category}
-          title={data[4].title}
-          link={data[4].link}
-        />
+        {data.map((obj) => (
+          <Card
+            imageSrc={obj.imageSrc}
+            category={obj.category}
+            title={obj.title}
+            link={obj.link}
+          />
+        ))}
       </div>
     </>
   );
