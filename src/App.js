b374df5b20518c8e110cc2b1b9fdf11f0e3880dd
Abhishek Yadav <abhishekyadav9844221505@gmail.com>
2020-09-13 17:51:45 +0545

    Compltetd the form part two and solved the form defsult behaviour

--- src/App.js
+++ src/App.js
@@ -2,26 +2,39 @@ import React, { useState } from "react";
 import "./App.css";
 function App() {
   const [userName, setUserName] = useState("");
+  const [userPassword, setUserPassword] = useState("");
   return (
-    <div className="container">
-      <h1>
-        Hello <span id="user"></span>
-      </h1>
-      <input
-        value={userName}
-        onChange={(event) => setUserName(event.target.value)}
-        placeholder="Enter your username..."
-        type="text"
-      />
-      <button
-        onClick={() => {
-          document.getElementById("user").innerHTML = userName;
-          setUserName("");
-        }}
-      >
-        Submit
-      </button>
-    </div>
+    <form>
+      <div className="container">
+        <h1>
+          Hello <span id="user"></span>
+        </h1>
+        <input
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)}
+          placeholder="Enter your username..."
+          type="text"
+        />
+        <input
+          value={userPassword}
+          onChange={(event) => setUserPassword(event.target.value)}
+          placeholder="Enter your password..."
+          type="password"
+        />
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            document.getElementById(
+              "user"
+            ).innerHTML = `${userName} ${userPassword}`;
+            setUserName("");
+            setUserPassword("");
+          }}
+        >
+          Submit
+        </button>
+      </div>
+    </form>
   );
 }
 
