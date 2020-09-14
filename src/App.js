cae605610c617fa8749cacb1cdb5d7440f8f849b
Abhishek Yadav <abhishekyadav9844221505@gmail.com>
2020-09-14 16:53:55 +0545

    Completed the form with email field with our own idea

--- src/App.js
+++ src/App.js
@@ -1,34 +1,63 @@
 import React, { useState } from "react";
 import "./App.css";
 function App() {
-  const [userName, setUserName] = useState("");
-  const [userPassword, setUserPassword] = useState("");
+  const [userDetail, setUserDetail] = useState({
+    userName: "",
+    userPassword: "",
+    userEmail: "",
+  });
   return (
     <form>
       <div className="container">
         <h1>
           Hello <span id="user"></span>
         </h1>
+        <h3 id="userEmail"></h3>
         <input
-          value={userName}
-          onChange={(event) => setUserName(event.target.value)}
+          value={userDetail.userName}
+          onChange={(event) =>
+            setUserDetail({
+              userName: event.target.value,
+              userPassword: userDetail.userPassword,
+              userEmail: userDetail.userEmail,
+            })
+          }
           placeholder="Enter your username..."
           type="text"
+          name="userName"
         />
         <input
-          value={userPassword}
-          onChange={(event) => setUserPassword(event.target.value)}
+          value={userDetail.userEmail}
+          onChange={(event) =>
+            setUserDetail({
+              userName: userDetail.userName,
+              userPassword: userDetail.userPassword,
+              userEmail: event.target.value,
+            })
+          }
+          placeholder="Enter your email..."
+          type="email"
+          name="userPassword"
+        />
+        <input
+          value={userDetail.userPassword}
+          onChange={(event) =>
+            setUserDetail({
+              userName: userDetail.userName,
+              userPassword: event.target.value,
+              userEmail: userDetail.userEmail,
+            })
+          }
           placeholder="Enter your password..."
           type="password"
+          name="userPassword"
         />
         <button
           onClick={(event) => {
             event.preventDefault();
-            document.getElementById(
-              "user"
-            ).innerHTML = `${userName} ${userPassword}`;
-            setUserName("");
-            setUserPassword("");
+            document.getElementById("user").innerHTML = userDetail.userName;
+            document.getElementById("userEmail").innerHTML =
+              userDetail.userEmail;
           }}
         >
           Submit
