d2cd375c4261ce42910ada288098341c4811fe90
Abhishek Yadav <abhishekyadav9844221505@gmail.com>
2020-10-10 13:24:42 +0545

    Added the context api in our way

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import Comp1 from "./Comp1";
 
 const Data = createContext();
 const Data2 = createContext();
 function App() {
+  const [Data1, setData1] = useState("Abhishek is a good boy");
   return (
     <>
-      <Data.Provider value="Abhishek is a good boy">
+      <Data.Provider value={[Data1, setData1]}>
         <Data2.Provider value="Abhishek is a bad boy">
           <Comp1 />
         </Data2.Provider>
--- src/Comp2.js
+++ src/Comp2.js
@@ -3,12 +3,16 @@ import Comp3 from "./Comp3";
 import { Data, Data2 } from "./App";
 
 function Comp2() {
-  const data1 = useContext(Data);
+  const [data1, setData1] = useContext(Data);
   const data2 = useContext(Data2);
   return (
     <div>
       <h1>{data1}</h1>
       <h1>{data2}</h1>
+      <button onClick={() => setData1("Abhishek is a very bad boy")}>
+        {" "}
+        Click me
+      </button>
       <h1>------------</h1>
       <Comp3 />
     </div>
