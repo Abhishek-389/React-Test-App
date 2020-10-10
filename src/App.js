0d04683223c65af338df9c1fe3eab8f43164c8b3
Abhishek Yadav <abhishekyadav9844221505@gmail.com>
2020-10-10 10:52:23 +0545

    Added the ui components

--- src/App.js
+++ src/App.js
@@ -2,23 +2,31 @@ import React, { useState } from "react";
 import "./App.css";
 import AddIcon from "@material-ui/icons/Add";
 import RemoveIcon from "@material-ui/icons/Remove";
+import Button from "@material-ui/core/Button";
+import Tooltip from "@material-ui/core/Tooltip";
+
 function App() {
   const [value, setValue] = useState(0);
   return (
     <div className="app">
       <div className="container">
         <span>{value}</span>
-        <div className="btn">
-          <button onClick={(event) => setValue(value + 1)}>
-            <AddIcon />
-          </button>
-          <button
-            onClick={(event) =>
-              value !== 0 ? setValue(value - 1) : alert("Reached zero")
-            }
-          >
-            <RemoveIcon />
-          </button>
+        <div>
+          <Tooltip title="Add">
+            <Button className="btn" onClick={(event) => setValue(value + 1)}>
+              <AddIcon />
+            </Button>
+          </Tooltip>
+          <Tooltip title="Sub">
+            <Button
+              className="btn"
+              onClick={(event) =>
+                value !== 0 ? setValue(value - 1) : alert("Reached zero")
+              }
+            >
+              <RemoveIcon />
+            </Button>
+          </Tooltip>
         </div>
       </div>
     </div>
