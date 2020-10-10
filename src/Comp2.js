c5677adf96cddef76bf0ab67ed38754837fbc8ea
Abhishek Yadav <abhishekyadav9844221505@gmail.com>
2020-10-10 13:18:50 +0545

    Added the useState hook

--- src/Comp2.js
+++ src/Comp2.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import Comp3 from "./Comp3";
+import { Data, Data2 } from "./App";
 
 function Comp2() {
+  const data1 = useContext(Data);
+  const data2 = useContext(Data2);
   return (
     <div>
+      <h1>{data1}</h1>
+      <h1>{data2}</h1>
+      <h1>------------</h1>
       <Comp3 />
     </div>
   );
