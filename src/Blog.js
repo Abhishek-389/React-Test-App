943e2f513d504e6bbe839bfc9e2d26d515effa4a
Abhishek Yadav <abhishekyadav9844221505@gmail.com>
2020-10-10 21:54:48 +0545

    Added useHistory

--- src/Blog.js
+++ src/Blog.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useHistory, useLocation, useParams } from "react-router-dom";
 
 function Blog(props) {
   const { writer, date } = useParams();
   const location = useLocation();
+  const history = useHistory();
   return (
     <div>
       <h1>
         This is the blog of {writer} on {date}
       </h1>
       <h3>Your location is {location.pathname}</h3>
+      <button onClick={() => history.goBack()}>Go back</button>
+      <button onClick={() => history.push("/")}>Go Home</button>
       {location.pathname === "/blog/Abhi/200"
         ? alert("You are the boss")
         : null}
