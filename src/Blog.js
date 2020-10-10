f6857fdc9b373760e4349110fcd4291ea603fae3
Abhishek Yadav <abhishekyadav9844221505@gmail.com>
2020-10-10 21:45:49 +0545

    Added the useParams

--- src/Blog.js
+++ src/Blog.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 function Blog(props) {
   const { writer, date } = useParams();
+  const location = useLocation();
   return (
     <div>
       <h1>
         This is the blog of {writer} on {date}
       </h1>
+      <h3>Your location is {location.pathname}</h3>
+      {location.pathname === "/blog/Abhi/2000"
+        ? alert("You are the boss")
+        : null}
     </div>
   );
 }
